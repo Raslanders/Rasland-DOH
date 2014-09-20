a1c2aac5df394086c6dacca3ca944934b086edb5
Rik <Rik.schreurs@mail.com>
2014-09-20 23:52:43 +0200

    added validate flight

--- backend/Schiphol.php
+++ backend/Schiphol.php
@@ -33,5 +33,28 @@ Class Schiphol {
         }       
     }
     
+    public function validate() {
+        $url = 'http://145.35.195.100/rest/flights/D/' . $this->flightNumber;
+        $opts = array(
+            'http'=>array(
+                'method'=>"GET",
+                'header'=>"Accept:application/json" 
+            )
+        );
+        $context = stream_context_create($opts);
+        $response = file_get_contents($url, false, $context);
+        $json = json_decode($response, true);
+        if(empty($json['Flights'])) {
+            //Fight does not exist or does not depart from Schiphol
+            return array('statusCode' => '404',
+                         'message' => 'Flight does not exist');
+        }
+        else {
+            //Flight is valid
+            return array('statusCode' => '200',
+                         'message' => 'Flight is valid');
+        } 
+    }
+    
     
 }
