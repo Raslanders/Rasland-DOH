9591253bbbd1ce43bfb5c72189fcd30fa278be7b
unknown <s112622@S112622.campus.tue.nl>
2014-09-20 21:20:36 +0200

    removing cf

--- backend/Transavia/transaviatest.php
+++ backend/Transavia/transaviatest.php
@@ -1,56 +1,88 @@
 <?php
-$url = "http://transavia-doh-api.jit.su:80/api/";
-$journeys = "journeys";
-$bookings = "bookings";
-$legs = "legs";
+$url = "TransaviaCopy2.csv";
 
-//print_r("Iteration 2");
+$transaviaArray = [];
 
-//$id = "541c209d27e5bc670de4395c";
-//$requestUrl =$url.$journeys."/".$id;
-//print_r($requestUrl);
-//$response = file_get_contents($requestUrl,false);
-//print_r($response);
+$flightNumber = "HV6583";
+$row = 0;
+$found =false ;
+$personsInGroup =[];
+$currentHighest = 0;
 
 
+if (($handle = fopen($url, "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 0, ",")) !== FALSE)    
+    {
+    //    print( $row);
+  //      print ("<br >");
+        if (($data[1].$data[2] == $flightNumber))
+        {
+            $digits = $data[3];
+            $year1 = substr($digits, -4);
+            $month1 = substr(substr($digits, -6),2);
+            $day1 = substr($digits,strlen($digits)-6);
 
-//CHECK IF FIRST EXISTS REQUIRED
-//$id = "541c33597cc3f3f8ac706358";
-$requestUrl =$url.$bookings;
-$response = file_get_contents($requestUrl,false);
+            $year2 = substr($currentHighest, -4);
+            $month2 = substr(substr($currentHighest, -6),2);
+            $day2 = substr($digits,strlen($currentHighest)-6);
 
-$arr = json_decode($response,true);
-
-//print_r($arr);
-
-
-$bookingIdArray = [];
-foreach($arr as $item) { //foreach element in $arr
-    array_push($bookingIdArray,$item['id']);
-}
-
-
-
-$resultingArray = [];//contains legId,bookingId
-
-foreach($bookingIdArray as $key => $value) { //foreach element in $arr
-$requestUrl =$url.$bookings."/".$value."/legs";
-$response = file_get_contents($requestUrl,false);
-    $arr = json_decode($response,true);
-    foreach($arr as $item) { //foreach element in $arr
-        $arrayToAdd = array("legId" => $item['id'],"bookingId" => $item['bookingId']);
-        $resultingArray = array_merge_recursive($resultingArray,$arrayToAdd);
+            //check if the date is more recent than the one we had in storage, if it is, remove the groups in storage and continue
+            if ($year1 < $year2) 
+            {
+                continue;
+            }
+            if ($year1 > $year2)
+            {
+                $currentHighest = $digits;
+            }
+            if ($year1 == $year2)
+            {
+                //check month now
+                if ($month1 < $month2) 
+                {
+                    continue;
+                }
+                if ($month1 > $month2)
+                {
+                    $currentHighest = $digits;
+                    $personsInGroup =[];
+                }
+                if ($month1 == $month2)
+                {
+                   //check day now
+                    if ($day1 < $day2) 
+                    {
+                        continue;
+                    }
+                    if ($day1 > $day2)
+                    {
+                        $currentHighest = $digits;
+                        $personsInGroup =[];
+                    }
+                }
+            }
+            //we have skipped if the date was lower or equal to the newest date.
+            
+            
+        
+            //we found our flight
+            //print_r($data[0].";".$data[1].$data[2].";".$data[3].$data[4]);
+            $found = true;
+            if(array_key_exists($data[0],$personsInGroup))
+            {
+            $personsInGroup[$data[0]] = $personsInGroup[$data[0]]+1;
+            }
+            else
+            {
+                $personsInGroup[$data[0]] = 1;
+            }
+            
+        }
+        $row++;
     }
+    print_r($personsInGroup);
+    fclose($handle);
 }
-
-
-        print_r($resultingArray);
-
-//flightcode HV5497
-//$fc = "HV5497";
-//$requestUrl =$url.$legs."/".$fc."/legs";
-//$response = file_get_contents($requestUrl,false);
-//print_r($response);
 
 
 
