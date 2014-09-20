6adea9ab0b99ffeddf542656caec9c52d326937e
Rik <Rik.schreurs@mail.com>
2014-09-20 23:40:25 +0200

    Checkin.php klaar

--- backend/Checkin.php
+++ backend/Checkin.php
@@ -1,19 +1,35 @@
 <?php
+require_once('Database/database.php');
 Class Checkin {
-    private $requestl;
+    private $request;
     function __construct($request) {
         $this->request = $request;
     }
     
     public function process() {
-        //Change boolean in database
-    }
-    
-    
-    
-    
-    
+        $database = new Database();
+        if(isset($this->request['id'])) {
+                $values = array($this->request['id']);
+                $result = $database->query("SELECT * FROM persons WHERE id= ?", $values, PDO::FETCH_ASSOC);
+
+            // Check if id is in database
+            if (sizeof($result) === 0) {
+                //Person is not in database
+                return array('statusCode' => '404',
+                             'message' => 'id was not found in the database');
+
+            } else {
+                //Person is in database
+                $values = array($this->request['id']);
+                $database->nquery("UPDATE persons SET CheckedIn = 1 WHERE id = ?", $values);
+                return array('statuscode' => '200',
+                             'message' => 'Checked in correctly');    
+            } 
+        }
+        else {
+            return array('statusCode' => '400',
+                         'message' => 'unique id was not provided');
+        }
+    }    
 }
-
-
 ?>
