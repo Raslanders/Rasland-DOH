7d70d0f935f0bafda106493661cd2f46e1c8be2b
unknown <s112622@S112622.campus.tue.nl>
2014-09-21 05:51:02 +0200

    sleepy sleepy

--- backend/HueMaster/Pitch.php
+++ backend/HueMaster/Pitch.php
@@ -3,14 +3,11 @@
 require_once ('huecontrol.php');
 
 $control = new HueControl();
-while(true){
-
 	$control->reset();
 	$control->turnOn(1);
 	$control->turnOn(2);
 	$control->turnOn(3);
-}
 
-
+//sleep(2);
 
 ?>
