202b2eabac5d2281af3a4f2c6f27b971ba194a57
Ruud Andriessen <ruud.andriessen@gmail.com>
2014-09-21 09:13:38 +0200

    Update to map merge conflict

--- frontend/js/maps.js
+++ frontend/js/maps.js
@@ -7,41 +7,6 @@ $(document).ready(function() {
   google.maps.event.addDomListener(window, 'load', mapInitialize);
 });
 
-
-<<<<<<< HEAD
-  function mapInitialize() {
-    var yourLatitude = 52.3101192;
-    var yourLongitude = 4.7681613;
-
-    var myOptions = {
-      zoom: 15,
-      center: new google.maps.LatLng(yourLatitude, yourLongitude - 0.01),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeControl: false,
-      panControl: false,
-      zoomControl: false,
-      scaleControl: false,
-      streetViewControl: false,
-      //styles: mapDark
-    };
-
-    map = new google.maps.Map(document.getElementById('google-map'), myOptions);
-    addMeetingPoint(lat,long);
-  }
-
-  function addMeetingPoint(lat, long) {    
-    var image = {
-      url: 'img/marker.png',
-    };
-    var myLatLng = new google.maps.LatLng(lat, long);
-    var myLocation = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: "Meeting point A"
-      //icon: image
-    });
-  }
-=======
 function mapInitialize() {
   var yourLatitude = 52.3101192;
   var yourLongitude = 4.7781613;
@@ -86,4 +51,3 @@ function addMeetingPoint(lat, long) {
     //icon: image
   });
 }
->>>>>>> 82229b627280866318016740ea7463e09ff92643
