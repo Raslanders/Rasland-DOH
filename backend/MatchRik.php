552f1b147a75e0c3e0777d9deaecf08fa8a67839
Rik <Rik.schreurs@mail.com>
2014-09-21 11:33:55 +0200

    yolo

--- backend/MatchRik.php
+++ backend/MatchRik.php
@@ -1,5 +1,6 @@
 <?php
 require_once('Database/database.php');
+require_once('HueMaster/HueControl.php');
 Class MatchRik {
     public function checkForMatches() {
         $database = new Database();
@@ -9,6 +10,7 @@ Class MatchRik {
         for($i=0; $i < sizeof($personsNotMatched); $i++) {
             array_push($values, $personsNotMatched[$i]['id']);
         }
+        if(sizeof($personsNotMatched)) {
         $qMarks = str_repeat('?,', sizeof($personsNotMatched) - 1) . '?';
 
         $persons = $database->query("SELECT * FROM persons WHERE id IN ( ".$qMarks .")", $values, PDO::FETCH_ASSOC);
@@ -21,7 +23,9 @@ Class MatchRik {
                         if($persons[$i]['Goal'] == $persons[$j]['Goal'] && $persons[$i]['Flightnumber'] == $persons[$j]['Flightnumber']) {
                             array_push($ignoreList, $i);
                             array_push($ignoreList, $j);
-                            $values = array($persons[$i]['id'], $persons[$j]['id'], "#FF0000", "A", 5);
+                            $color = $this->getColor();
+                            $matchNumber = $this->getMatchNumber();
+                            $values = array($persons[$i]['id'], $persons[$j]['id'], $color, "A", $matchNumber);
                             $database->nquery("INSERT INTO matches (uid1, uid2, color, poleName, matchNumber) VALUES (?, ?, ?, ?, ?)", $values);
                         }
                     }
@@ -29,8 +33,71 @@ Class MatchRik {
             }
         }
     
+        }
+    }
+    
+    private function getColor() {
+        $database = new Database();
+        $randomNumber = rand(0, 2);
+        $colors = array(0 => 'green',
+                         1 => 'orange',
+                         2 => 'blue', 
+                         /*3 => 'orange',
+                         4 => 'yellow',
+                         5 => 'purple'*/);
+        //$this->changeColor($colors[$randomNumber]);
+        return $colors[$randomNumber];       
         
     }
+    
+    private function getMatchNumber() {
+        $database = new Database();
+        return rand(1,999);
+        
+    }
+    
+    private function changeColor( $colorname ) {
+        $command;
+        switch ( $colorname )
+        {
+            case "green":
+            $command = 26000;
+            break;
+
+            case "orange":
+            $command = 9100;
+            break;
+
+            case "blue":
+            $command = 45000;
+            break;
+
+            /*case "orange":
+            $command = 9100;
+            break;
+
+            case "yellow":
+            $command = 18000;
+            break;
+            case "purple":
+            $command = 52000;
+            break;*/
+        }
+        try {
+        $control = new HueControl();
+        $control->turnOn(1);
+        $control->turnOn(2);
+        $control->turnOn(3);
+        $control->setColor(1, 26000);
+        $control->setColor(2, 0);
+        $control->setColor(3, 45000);
+        }
+        catch (Exception $e) {
+            
+        }
+        return $colorname;
+    }
+
     
     
 }
